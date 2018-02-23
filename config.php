75ed3cb45a91f1c011d10249c7904f4d6e6df936
Somyot Srihanon <somyot.srihanon@gmail.com>
2018-02-23 19:06:22 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 error_reporting(0);
 session_start();
 
-header("Access-Control-Allow-Origin:*"); // อนูญาตให้โปรแกรมอื่นสามารถดึงข้อมูลได้
-header("Content-Type: application/json; charset=UTF-8"); // แสดงภาษาไทย
+//header("Access-Control-Allow-Origin:*"); // อนูญาตให้โปรแกรมอื่นสามารถดึงข้อมูลได้
+//header("Content-Type: application/json; charset=UTF-8"); // แสดงภาษาไทย
 
 //การกำหนดค่าฐานข้อมูล
 define('DB_SERVER', 'localhost');
